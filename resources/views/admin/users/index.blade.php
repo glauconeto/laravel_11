ee2c4a4bdd9552e44f73a489e97cc68676ecf19b
glauconeto <glauconeto@tutanota.com>
2025-02-11 19:25:23 -0300

    Geração de usuários

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>>
+</head>
+<body>
+    <h1>Usuários</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>---</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="100">Nenhum usuário registrado</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{ $users->links() }}
+</body>
+</html>
