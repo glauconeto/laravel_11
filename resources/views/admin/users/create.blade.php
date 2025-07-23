55009c98d070943db83e09bcd280d6545bb6018f
glauconeto <glauconeto@tutanota.com>
2025-07-23 14:40:18 -0300

    Criado estilos com tailwindcss

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,10 +1,15 @@
 @extends('admin.layouts.app')
 
-@section('title', 'Criar usuário')
+@section('title', 'Criar Novo Usuário')
 
 @section('content')
-    <h1>Novo usuário</h1>
-
+    @include('admin.users.partials.breadcrumb')
+    <div class="py-6">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
+            Novo Usuário
+        </h2>
+    </div>
+    {{-- @include('admin.includes.errors') --}}
     <form action="{{ route('users.store') }}" method="POST">
         @include('admin.users.partials.form')
     </form>
